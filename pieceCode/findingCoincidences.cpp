89199a4bcf59d675c491aa6a0e695a0bc91c6eca
thejollyfatso <rafael.r.deleon@gmail.com>
2023-10-08 13:49:51 -0700

    documentation added

--- pieceCode/findingCoincidences.cpp
+++ pieceCode/findingCoincidences.cpp
@@ -1,3 +1,8 @@
+/*
+  This code is used to guess the length of the key.
+  Given a cipher text, it will generate a cascading list and calculate coincidences.
+  Key length will be guessed based on spacing between outliers.
+*/
 #include <iostream>
 #include <vector>
 #include <string>
---
+++ pieceCode/frequencyAnalysis.cpp
@@ -0,0 +1,15 @@
+/*
+  This code follows findingCoincidences.cpp
+  Given the same cipher text and the guessed key length,
+  frequency analysis will determine the most likely key.
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std
+
+int main()
+{
+  return 0;
+}
