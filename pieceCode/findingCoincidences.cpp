f6a9874e67ecd6155944e8f4552bd7e92a471292
thejollyfatso <rafael.r.deleon@gmail.com>
2023-10-08 14:03:30 -0700

    basic coincidence counting code created, untested

--- pieceCode/findingCoincidences.cpp
+++ pieceCode/findingCoincidences.cpp
@@ -9,7 +9,32 @@
 
 using namespace std
 
+// Functions
+string genShiftedString (string ogCipher, int shift);
+int countCoincidences (string ogCipher, string cmpString);
+
 int main()
 {
   return 0;
+}
+
+string genShiftedString (string ogCipher, int shift)
+{
+  string shiftString;
+  int j = 0;
+  for ( int i = shift; i < ogCipher.length(); i++ )
+  {
+    shiftString[j++] = ogCipher[i];
+  }
+  return shiftString;
+}
+
+int countCoincidences (string ogCipher, string cmpString)
+{
+  int cnt = 0;
+  for ( int i = 0; i < cmpString.length(); i++ )
+  {
+    if ogCipher[i] == cmpString[i] cnt++;
+  }
+  return cnt;
 }
