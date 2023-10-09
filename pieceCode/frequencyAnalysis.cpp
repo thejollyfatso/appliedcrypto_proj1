d8e6d5a0829b5afb7b98014572234eb5c0855f00
thejollyfatso <rafael.r.deleon@gmail.com>
2023-10-08 21:33:56 -0700

    frequency analysis has dictionaries and reads substrings

--- pieceCode/frequencyAnalysis.cpp
+++ pieceCode/frequencyAnalysis.cpp
@@ -10,18 +10,25 @@
 
 using namespace std;
 
-// Functions
+// Globals
+string LSYMBOLS = " abcdefghijklmnopqrstuvwxyz";
+
+/* Function Declarations */
+vector<int> genLSymbolFrequency (const string lstring);
+vector<int> genShiftedVector (const vector<int> ogVector, const int shift);
 string genShiftedString (const string ogCipher, const int shift);
 int countCoincidences (const string ogCipher, const string cmpString, const int shift);
-vector<int> genKeyLengthGuesses(const vector<int> coincidenceCount, const int maxLength);
+vector<int> genKeyLengthGuesses (const vector<int> coincidenceCount, const int maxLength);
 
 int main()
 {
-  // read in file
+  // read in ciphertext
   ifstream ifs("cipher.txt");
   string cText( ( istreambuf_iterator<char>(ifs) ),
                 ( istreambuf_iterator<char>() ) );
+  ifs.close();
 
+  /* Coincidence Indexing to determine likely key lengths */
   // for length of string, count coincidences of resulting shift of i
   vector<int> coincidences;
   for ( int i = 0; i < cText.length(); i++ )
@@ -33,7 +40,67 @@ int main()
   // generate key length guesses based on coincidences and factors
   vector<int> keyLengthCandidates = genKeyLengthGuesses(coincidences, cText.length());
 
+  /* Frequency Analysis to guess keys */
+  // get true frequency from dictionaries
+  // read dictionaries into strings
+  ifstream ifs2("plaintext_dictionary_test1.txt");
+  string dict1( ( istreambuf_iterator<char>(ifs) ),
+                ( istreambuf_iterator<char>() ) );
+  ifs2.close();
+  ifstream ifs3("plaintext_dictionary_test2.txt");
+  string dict2( ( istreambuf_iterator<char>(ifs) ),
+                ( istreambuf_iterator<char>() ) );
+  ifs3.close();
+  // get dict1,dict2 frequencies then combine into first
+  vector<int> trueFreq = genLSymbolFrequency(dict1);
+  vector<int> dict2Freq = genLSymbolFrequency(dict2);
+  for ( int i = 0; i < 27; i++ )
+  {
+    trueFreq[i] += dict2Freq[i];
+  }
+
+  // get frequencies from possible keys
+  for ( auto length : keyLengthCandidates )
+  {
+    for ( int i = 0; i < length; i++ )
+    {
+      string substring;
+      for ( int j = i; j < cText.length(); j += length )
+      {
+        // for every length-th element add to substring
+        substring += cText[j];
+      }
+
+      // analyze substr freq
+      vector<int> substrFreq = genLSymbolFrequency(substring);
+
+    }
+  }
+
   return 0;
+}
+
+/* Function Definitions */
+
+vector<int> genLSymbolFrequency (const string lstring)
+{
+  vector<int> freqs(27);
+  int lIndex; // ascii converted to index within LSYMBOLS
+  for ( auto c : lstring )
+  {
+    if ( LSYMBOLS.find(c) != string::npos )
+    {
+      lIndex = (c == ' ') ? 0 : (int(c) - 'a' + 1);
+      freqs[lIndex] += 1;
+    }
+  }
+
+  return freqs;
+}
+
+vector<int> genShiftedVector (const vector<int> ogVector, const int shift)
+{
+  return ogVector; // obv DEBUG
 }
 
 string genShiftedString (const string ogCipher, const int shift)
@@ -57,7 +124,7 @@ int countCoincidences (const string ogCipher, const string cmpString, const int 
   return cnt;
 }
 
-vector<int> genKeyLengthGuesses(const vector<int> coincidenceCount, const int maxLength)
+vector<int> genKeyLengthGuesses (const vector<int> coincidenceCount, const int maxLength)
 {
   vector<int> candidateGuesses;
   vector<int> guesses;
